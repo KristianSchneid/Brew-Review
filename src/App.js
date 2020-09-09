17617afa93d3a30753b67099bc5f5d141005140e
unknown <thomas>
2020-09-09 14:06:58 +0100

    cleaned basic project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import VisitorHome from "./components/Visitor Home.js"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Brew Review Mate
+          <VisitorHome />
         </a>
       </header>
     </div>
---
+++ src/components/Visitor Home.js
@@ -0,0 +1,8 @@
+
+import React from "react";
+
+export default class VisitorHome extends React.Component {
+    render() {
+        return <h1>VisitorHome</h1>;
+    }
+}
